a47b9808be022250985814f3851f26b1da7a4238
Niks4925 <nicolas.larmonier@gmail.com>
2021-05-25 21:20:21 +0100

    Fix pseudos selector as described by roger21

--- [HFR] Bookmarks.user.js
+++ [HFR] Bookmarks.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name          [HFR] Bookmarks
-// @version       0.5
+// @version       0.6
 // @namespace     forum.hardware.fr
 // @description   Gestion de posts favoris avec stockage en MPStorage
 // @icon          data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAMAAABEpIrGAAAAilBMVEX%2F%2F%2F8AAADxjxvylSrzmzf5wYLzmjb%2F9er%2F%2Fv70nj32q1b5woT70qT82rT827b%2F%2B%2FjxkSHykybykyfylCjylCnzmDDzmjX0nTv1o0b1qFH2qVL2qlT3tGn4tmz4uHD4uXL5vHf83Lf83Lj937394MH%2B587%2B69f%2F8%2BX%2F8%2Bf%2F9On%2F9uz%2F%2BPH%2F%2BvT%2F%2FPmRE1AgAAAAwElEQVR42s1SyRbCIAysA7W2tdZ93%2Ff1%2F39PEtqDEt6rXnQOEMhAMkmC4E9QY9j9da1OkP%2BtTiBo1caOjGisDLRDANCk%2FVIHwwkBZGReh9avnGj2%2FWFg%2Feg5hD1bLZTwqdgU%2FlTSdrqZJWN%2FKImPOnGjiBJKhYqMvikxtlhLNTuz%2FgkxjmJRRza5mbcXpbz4zldLJ0lVEBY5nRL4CJx%2FMEfXE4L9j4Qr%2BZakpiandMpX6FO7%2FaPxxUTJI%2FsJ4cd4AoSOBgZnPvgtAAAAAElFTkSuQmCC
@@ -238,7 +238,7 @@ LocalMPStorage.initBLMPStorage().then(function() {
         }
 
        if(postId > 0 && (catId > 0 || catId == "prive")){
-           let pseudos = root.querySelectorAll("table.messagetable > tbody > tr > td.messCase2 > div.toolbar > div.left");
+           let pseudos = root.querySelectorAll("table.messagetable > tbody > tr > td.messCase2 > div.toolbar > div.left:first-of-type");
            for(let pseudo of pseudos) {
                let imgBK = document.createElement("img");
                imgBK.setAttribute("src", img_bookmarks);
