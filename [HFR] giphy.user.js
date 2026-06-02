334b81cf79f6456dd6ff740bba99a0b131814900
roger21 <roger21@free.fr>
2026-06-02 09:02:12 +0200

    7tv (#4)
    * ajout de 7tv

--- [HFR] giphy.user.js
+++ [HFR] giphy.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         [HFR] Giphy
-// @version      0.4.3
+// @version      0.5
 // @namespace    http://tampermonkey.net/
-// @description  Ajoute la recherche et l'insertion de gifs via Giphy et Tenor
+// @description  Ajoute la recherche et l'insertion de gifs via Giphy, Tenor et 7tv
 // @author       Garath_
 // @match        https://forum.hardware.fr/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=hardware.fr
@@ -13,7 +13,7 @@
 (function() {
     'use strict';
 
-class GifService {
+    class GifService {
         #apiKey;
 
         constructor(endpoint) {
@@ -56,7 +56,7 @@ class GifService {
             gifData.forEach(gif => {
                 const image = document.createElement('img');
                 image.src = gif.images.fixed_width_small.url;
-                image.style.margin = "5px";
+                image.style.margin = "0 5px 5px 0";
 
                 image.addEventListener('click', () => {
                     this.addGifToForm(gif.images.downsized.url);
@@ -96,10 +96,98 @@ class GifService {
             gifData.forEach(gif => {
                 const image = document.createElement('img');
                 image.src = gif.media_formats.nanogif.url;
-                image.style.margin = "5px";
+                image.style.margin = "0 5px 5px 0";
 
                 image.addEventListener('click', () => {
                     this.addGifToForm(gif.media_formats.webp.url);
+                });
+
+                container.appendChild(image);
+            });
+        }
+    }
+
+    class Seven extends GifService {
+        #apiKey = "prout";
+        #ended = false;
+        #next = 1;
+
+        constructor() {
+            super("https://7tv.io/v4/gql");
+        }
+
+        async search(query, offset, container) {
+            if (offset == 0) {
+                this.#ended = false;
+                this.#next = 1;
+            }
+
+            if (this.#ended) {
+                return;
+            }
+
+            try {
+                const response = await fetch(this.endpoint, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({
+                        query: `query seven($soso: Sort!, $fifi: Filters) {
+                          emotes {
+                            search(
+                              query: "${query}"
+                              perPage: 50
+                              page: ${this.#next}
+                              sort: $soso
+                              filters: $fifi
+                            ) {
+                              items {
+                                id
+                                defaultName
+                                aspectRatio
+                              }
+                              totalCount
+                              pageCount
+                            }
+                          }
+                        }`,
+                        variables: {
+                            soso: {
+                                sortBy: "TOP_ALL_TIME",
+                                order: "DESCENDING"
+                            },
+                            fifi: {
+                                exactMatch: false
+                            }
+                        }
+                    })
+                });
+
+                const data = await response.json();
+                this.#ended = this.#next === data.data.emotes.search.pageCount;
+                ++this.#next;
+                this._displayGifs(data.data.emotes.search.items, container);
+            } catch (error) {
+                console.error("Erreur lors de la recherche Seven:", error);
+                return [];
+            }
+        }
+
+        _displayGifs(gifData, container) {
+            gifData.forEach(gif => {
+                const image = document.createElement('img');
+                image.src = "https://cdn.7tv.app/emote/" + gif.id + "/4x.avif";
+                image.setAttribute("title", gif.defaultName);
+                image.setAttribute("loading", "lazy");
+                image.style.margin = "0 5px 5px 0";
+
+                image.addEventListener('click', () => {
+                    this.addGifToForm(image.getAttribute("src"));
+                });
+
+                image.addEventListener('error', () => {
+                    image.src = "https://cdn.7tv.app/emote/" + gif.id + "/4x.avif?retry=" + Date.now();
                 });
 
                 container.appendChild(image);
@@ -114,11 +202,13 @@ class GifService {
 
     const giphyService = new Giphy();
     const tenorService = new Tenor();
+    const sevenService = new Seven();
 
     let gif = document.createElement("div");
     gif.setAttribute("id", "gif");
 
     const serviceSelector = document.createElement('select');
+    serviceSelector.style.margin = "0 5px 0 0";
     const giphyOption = document.createElement('option');
     giphyOption.value = 'giphy';
     giphyOption.textContent = 'Giphy';
@@ -127,19 +217,20 @@ class GifService {
     tenorOption.value = 'tenor';
     tenorOption.textContent = 'Tenor';
 
+    const sevenOption = document.createElement('option');
+    sevenOption.value = 'seven';
+    sevenOption.textContent = '7tv';
+
     serviceSelector.appendChild(giphyOption);
     serviceSelector.appendChild(tenorOption);
-    serviceSelector.addEventListener('change', function() {
-        results.innerHTML = '';
-        if (input_text.value.trim() !== "") {
-            getService().search(input_text.value, 0, results);
-        }
-    });
+    serviceSelector.appendChild(sevenOption);
+    serviceSelector.addEventListener('change', update, false);
 
     let results = document.createElement("div");
+    results.style.margin = "5px 0 0 0";
     let input_text = document.createElement("input");
     input_text.setAttribute("type", "text");
-    input_text.style = "vertical-align: bottom";
+    input_text.style.verticalAlign = "bottom";
     input_text.addEventListener('keyup', delay(update, 500), false);
     input_text.addEventListener('paste', delay(update, 500), false);
 
@@ -147,7 +238,7 @@ class GifService {
         const isBottom = results.scrollTop + results.clientHeight >= results.scrollHeight - 5;
         if (isBottom) {
             const offset = results.children.length;
-            getService().search(input_text.value, offset, results);
+            getService().search(input_text.value.trim(), offset, results);
         }
     });
     gif.appendChild(serviceSelector);
@@ -162,6 +253,8 @@ class GifService {
             return giphyService
         } else if (selectedService === 'tenor') {
             return tenorService
+        } else if (selectedService === 'seven') {
+            return sevenService
         }
     }
 
@@ -173,15 +266,16 @@ class GifService {
         }
     }
 
-    function update(){
+    function update() {
         results.innerHTML = '';
-        if (input_text.value.length >= 3) {
-            results.style.overflowY = "scroll";
-            results.style.height = "300px";
-            getService().search(input_text.value, 0, results);
-        }
-        else {
+        if (input_text.value.trim().length >= 3) {
+            results.style.overflowY = "auto";
+            results.style.maxHeight = "300px";
+            results.style.height = "auto";
+            getService().search(input_text.value.trim(), 0, results);
+        } else {
             results.style.overflowY = null;
+            results.style.maxHeight = null;
             results.style.height = null;
         }
     }
